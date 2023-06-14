5831ad6035b424b8191832253878b9d2d9e01f2a
Mieszko K <kuczmieszko@gmail.com>
2023-06-14 22:02:36 +0200

    Dodanie funckji wygeneruj_raport

--- salary.py
+++ salary.py
@@ -18,15 +18,36 @@ class Pracownik:
         koszt_pracowdawcy = round(self.wyplata_brutto + skladki_pracodawca, 2)
         return koszt_pracowdawcy
 
+    def wygeneruj_raport(self):
+        skladka_emeryt = 0.0976 * self.wyplata_brutto
+        skladka_rentowa = 0.015 * self.wyplata_brutto
+        skladka_chorobowa = 0.0245 * self.wyplata_brutto
+        skladka_zdrowotna = 0.09 * (self.wyplata_brutto - skladka_emeryt - skladka_rentowa - skladka_chorobowa)
+        skladka_rentowa_pracodawca = 0.065 ** self.wyplata_brutto
+        skladka_ubezp_pracodawca = 0.0167 * self.wyplata_brutto
+        skladka_funduszpracy = 0.0245 * self.wyplata_brutto
+        skladka_funduszgsp = 0.001 * self.wyplata_brutto
+        print("Wynagrodzenie brutto:", self.wyplata_brutto,
+              "\n\nSkładki płatne przez pracownika:\nSkładka emerytalna:", skladka_emeryt,
+              "\nSkładka rentowa:", skladka_rentowa,
+              "\nSkładka chorobowa:", skladka_chorobowa,
+              "\nSkładka zdrowotna:", skladka_zdrowotna,
+              "\n\nSkładki płatne przez pracodawcę:\nSkładka emerytalna:", skladka_emeryt,
+              "\nSkładka rentowa:", skladka_rentowa_pracodawca,
+              "\nSkładka na ubezpieczenie wypadkowe:", skladka_ubezp_pracodawca,
+              "\nSkładka na Fundusz Pracy:", skladka_funduszpracy,
+              "\nSkładka na Fundusz Gwarantowanych Świadczeń Pracowniczych:", skladka_funduszgsp
+              )
+
+
 
 Pracownik1 = Pracownik("Popiol", 3500)
 #Test funkcji oblicz_netto()
-#assert Pracownik1.oblicz_netto() == 2715.94, "Niepoprawna wypata"
+assert Pracownik1.oblicz_netto() == 2715.94, "Niepoprawna wypata"
 #Test funkcji __str__()
-#assert str(Pracownik1) == "Popiol 3500", "Nieoczekiwany string"
+assert str(Pracownik1) == "Popiol 3500", "Nieoczekiwany string"
 
-#poppozycja poprawki funkcji str:
-# return f"{self.nazwisko } {self.wyplata_brutto}"
 
 #Test funkcji oblicz_koszty_pracodawcy()
 assert Pracownik1.oblicz_koszty_pracodawcy() == 4216.8
+
